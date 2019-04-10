04b6ef0251f3163f263aea66a5c3b93b1abe4060
Ubuntu <ubuntu@ip-172-31-6-111.us-east-2.compute.internal>
2019-04-10 07:04:25 +0000

    modified message

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http=require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Darshan Raval');
+  res.end('Hello Darshan Raval. Hope you are doing good...!!! Thanks for creating CI/CD pipeline');
 }).listen(8080);
