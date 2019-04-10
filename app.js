ba793e89cb294ae49b9c8e4869483808636995f4
Ubuntu <ubuntu@ip-172-31-6-111.us-east-2.compute.internal>
2019-04-10 07:39:17 +0000

    latest modified

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ var http=require('http');
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello Darshan Raval. Hope you are doing good');
+  res.end('Hello Darshan');
 }).listen(8080);
